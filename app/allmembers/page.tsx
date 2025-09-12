32cd1945d1f3fbf3cf50f8b5a07f897b532d3355
kzVerif <165546547+kzVerif@users.noreply.github.com>
2025-09-12 23:56:24 +0700

    23

--- app/allmembers/page.tsx
+++ app/allmembers/page.tsx
@@ -1,7 +1,7 @@
 import { columns, Payment } from "./columns"
 import { DataTable } from "./data-table"
 export default async function DemoPage() {
-  const res = await fetch("/api/subscriptions", {
+  const res = await fetch("https://fast-fit-gym.vercel.app/api/subscriptions", {
     cache: "no-store",
   })
   const data: Payment[] = await res.json()
